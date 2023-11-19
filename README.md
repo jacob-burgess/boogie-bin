e597b0820166886665b0154971e946671490d1ab
Jacob Burgess <jacobburgess11@gmail.com>
2023-11-18 21:25:56 -0800

    init

---
+++ README.md
@@ -0,0 +1,51 @@
+# boogie bin
+
+collection of little scripts to make my life easier
+
+this all started when i was setting up my new computer and was fudgin around with .zshrc and $PATHs and what not and i kept forgetting how to pretty-print my path... i must have googled it 10 times. Finally i decided i was going to write a [script to do it for me](./cmd/path). Then i thought, "hey, i could write a script for all the things i google all the time." So I added the python venv stuff b/c i hate having to remember those ones
+
+I plan to add stuff i find myself forgetting. if there are suggestions, bang my boogie line.
+
+but i have no clue how to write code in bash, so idk why i did that. kinda fun tho.
+
+This is probably not the right way to do this kind of thing but its working, so im doing it.
+
+## to use this
+
+First of all, you probably dont want to use this. Its not really meant for anyone else to use. But if you do, here's how:
+
+1. clone this repo into your /Documents folder
+   - You dont really want it in your normal places you put code becuase we'll be adding it to your path and that just feels funny to me. it does really matter, its just an icky feeling
+2. add the directory to your path
+   - `cd && nano .zshrc`
+   - add `export PATH=$PATH:/Users/<your_username>/Documents/boogie-bin` to the end of the file
+   - save and close (ctrl+o, ENTER, ctrl+x)
+3. restart your terminal
+4. run `boogie` to confirm it works
+
+## adding a new script
+
+1. create a file without a file extenstion in the `/cmd` directory. The file name will be the cli command. e.g. a file named `picker` will be run with `boogie picker`
+2. Decide which runtime you want to write the script and determine its filepath
+   - run `which node`, `which bash`, etc. to get its filepath
+3. begin your script file with `#! <filepath_for_runtime>`, e.g. `#! /bin/bash`
+4. write your script
+5. `chmod +x <filename>`
+6. maybe have to restart the terminal, idfk
+7. test it out from anywhere in your terminal with `boogie <filename>`
+
+## make it your own
+
+I made this for me (boogie), but if you want to de-boogify, this section is for you.
+
+### basic idea
+
+The fact that `boogie-bin` is now in your path, any script you place in the root directory can be run directly from the command line. For example, you can add a script called `dingdong` to the root of your `boogie-bin` directory and run it simply by typing `dingdong` into your terminal.
+
+### de-boogify
+
+1. change the name of [boogie](./boogie) to something else, and you can run it with that name.
+2. If you want to change the name of the directory to something other than `boogie-bin`, you can do that too. Just make sure you update the path in your .zshrc file.
+3. Get yourself some ascii art to replace the boogie one in the [utilities.sh file](./scripts/utilities.sh). I used [patorjk.com](http://patorjk.com/software/taag/) s/o to [this guy](https://github.com/patorjk)
+
+I think that's all the boogies. Theres proabably more, but those are the big ones - you can go digging for your own boogies.
---
+++ scripts/utilities.sh
@@ -0,0 +1,131 @@
+#!/bin/bash
+
+# Define color/text variables
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+YELLOW='\033[0;33m'
+CYAN='\033[1;96m'
+PURPLE='\033[0;35m'
+BLUE='\033[0;34m'
+WHITE='\033[1;37m'
+RESET='\033[0m'
+BOLD='\033[1m'
+
+# Function to print a message with color
+p() {
+    local message="$1"
+    local color="$2"
+    if [ -z "$color" ]; then
+        color="$WHITE"
+    fi
+    echo -en "${color}${message}${RESET}"
+}
+
+# Function to print a message with color and a newline
+p_ln() {
+    p "$@"
+    echo ""
+}
+
+# Function to print a big boogie
+big_boogie() {
+
+  local color="$1"
+  if [ -z "$color" ]; then
+      color="$PURPLE"
+  fi
+
+  center_text "                                                                                                      " "$color";
+  center_text "bbbbbbbb                                                                                              " "$color";
+  center_text "b::::::b                                                                    iiii                      " "$color";
+  center_text "b::::::b                                                                   i::::i                     " "$color";
+  center_text "b::::::b                                                                    iiii                      " "$color";
+  center_text " b:::::b                                                                                              " "$color";
+  center_text " b:::::bbbbbbbbb       ooooooooooo      ooooooooooo      ggggggggg   gggggiiiiiii     eeeeeeeeeeee    " "$color";
+  center_text " b::::::::::::::bb   oo:::::::::::oo  oo:::::::::::oo   g:::::::::ggg::::gi:::::i   ee::::::::::::ee  " "$color";
+  center_text " b::::::::::::::::b o:::::::::::::::oo:::::::::::::::o g:::::::::::::::::g i::::i  e::::::eeeee:::::ee" "$color";
+  center_text " b:::::bbbbb:::::::bo:::::ooooo:::::oo:::::ooooo:::::og::::::ggggg::::::gg i::::i e::::::e     e:::::e" "$color";
+  center_text " b:::::b    b::::::bo::::o     o::::oo::::o     o::::og:::::g     g:::::g  i::::i e:::::::eeeee::::::e" "$color";
+  center_text " b:::::b     b:::::bo::::o     o::::oo::::o     o::::og:::::g     g:::::g  i::::i e:::::::::::::::::e " "$color";
+  center_text " b:::::b     b:::::bo::::o     o::::oo::::o     o::::og:::::g     g:::::g  i::::i e::::::eeeeeeeeeee  " "$color";
+  center_text " b:::::b     b:::::bo::::o     o::::oo::::o     o::::og::::::g    g:::::g  i::::i e:::::::e           " "$color";
+  center_text " b:::::bbbbbb::::::bo:::::ooooo:::::oo:::::ooooo:::::og:::::::ggggg:::::g i::::::ie::::::::e          " "$color";
+  center_text " b::::::::::::::::b o:::::::::::::::oo:::::::::::::::o g::::::::::::::::g i::::::i e::::::::eeeeeeee  " "$color";
+  center_text " b:::::::::::::::b   oo:::::::::::oo  oo:::::::::::oo   gg::::::::::::::g i::::::i  ee:::::::::::::e  " "$color";
+  center_text " bbbbbbbbbbbbbbbb      ooooooooooo      ooooooooooo       gggggggg::::::g iiiiiiii    eeeeeeeeeeeeee  " "$color";
+  center_text "                                                                  g:::::g                             " "$color";
+  center_text "                                                      gggggg      g:::::g                             " "$color";
+  center_text "                                                      g:::::gg   gg:::::g                             " "$color";
+  center_text "                                                       g::::::ggg:::::::g                             " "$color";
+  center_text "                                                        gg:::::::::::::g                              " "$color";
+  center_text "                                                          ggg::::::ggg                                " "$color";
+  center_text "                                                             gggggg                                   " "$color";
+  printf "\n"
+}
+
+center_text() {
+    local text="$1"
+    local color="$2"
+    local terminal_width=$(tput cols)
+
+    while IFS= read -r line; do
+        local padding=$(( (terminal_width - ${#line}) / 2 ))
+        echo -e "$(printf "%*s" $padding)${color}${line}${RESET}"
+    done <<< "$text"
+}
+
+# Function to check if a script is executable
+is_executable() {
+    local script="$1"
+    [ -x "$script" ]
+}
+
+# Function to print a number of spaces
+pad() {
+    local count="$1"
+    printf "%-${count}s" ""
+}
+
+# Function to check if directories exist
+check_directories() {
+    for dir in "$@"; do
+        if [ ! -d "$dir" ]; then
+            print_colored_message "Error: The directory \"$dir\" does not exist." "$RED"
+            exit 1
+        fi
+    done
+}
+
+# Function to get the description from a script
+get_description() {
+    local script_file="$1"
+    local description=""
+
+    # Check for Bash-style comments
+    local bash_description=$(grep -E "^#\s*Description:\s*" "$script_file" | sed -E 's/^# Description: //')
+    if [ -n "$bash_description" ]; then
+        description="$bash_description"
+    else
+        # Check for Node-style comments
+        local node_description=$(grep -E "^\/\/\s*Description:\s*" "$script_file" | sed -E 's/^\/\/ Description: //')
+        if [ -n "$node_description" ]; then
+            description="$node_description"
+        fi
+    fi
+
+    echo "$description"
+}
+
+# Function to run a command script
+run_command() {
+    local cmd_filename="$1"
+    local cmd_dir="$2"
+    local cmd_path="$cmd_dir/$cmd_filename"
+
+    if [ -f "$cmd_path" ]; then
+        "$cmd_path"
+    else
+        print_colored_message "Error: Command \"$cmd_filename\" not found in \"$cmd_dir\"." "$RED"
+        exit 1
+    fi
+}
